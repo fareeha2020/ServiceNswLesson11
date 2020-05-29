e510d645193b343b4eeb62ff2fd37a05bcf397f8
Fareeha Farhath <fareeha.farhath@tafensw.edu.au>
2020-05-29 15:48:36 +1000

    changed

---
+++ Exercises/ETdd.js
@@ -0,0 +1,4 @@
+// TDD
+// Create test cases for a function called multiplyNumbers that should take 2 numbers as parameters and return the product of those numbers.
+// Create the multiplyNumbers function such that it passes the tests written in Q1
+
---
+++ Exercises/Key.js
@@ -0,0 +1,3 @@
+module.exports={
+    myKey="439d4b804bc8187953eb36d2a8c26a02"
+}
